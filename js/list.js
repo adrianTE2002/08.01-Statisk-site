e5d89e9b0f5762edc14adb46e9479ba59fb4dc2f
Adrian Egeberg <adeg0001@stud.ek.dk>
2026-03-02 12:18:44 +0100

    Add product listing functionality with dynamic loading and discount display

---
+++ js/list.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const productContainer = document.querySelector("main");
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((response) => response.json())
+  .then((data) => {
+    showProducts(data);
+  });
+
+function showProducts(productsArr) {
+  //   console.log("productsArr:", productsArr);
+
+  productsArr.forEach((product) => {
+    console.log("product:", product.category);
+    const hasDiscount = product.discount > 0;
+
+    productContainer.innerHTML += `<article class="${product.soldout ? "soldOut" : ""}">
+      ${hasDiscount ? `<span class="discount_badge">-${product.discount}%</span>` : ""}
+    <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}">
+    <p class="soldoutTxt color_me_gray">SOLD OUT</p>
+    <h2>${product.productdisplayname}</h2>
+    <p>${product.articletype} - ${product.brandname}</p>
+    <p>DKK ${product.price},-</p>
+     <div class="discounted_element">
+        ${
+          hasDiscount
+            ? `
+          <span class="discount_badge">-${product.discount}%</span>
+          <p class="discounted_price">Now DKK ${Math.round(product.price * (1 - product.discount / 100))},-</p>
+        `
+            : ""
+        }
+        <a href="produkt.html">Read more</a>
+      </div>
+    </article>`;
+  });
+}
