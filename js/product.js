b63d0fcecb0b722017600e294fbbadd4f07eeba5
Adrian Egeberg <adeg0001@stud.ek.dk>
2026-03-05 23:42:44 +0100

    Updateret html og færdiggjort produkt js

--- js/product.js
+++ js/product.js
@@ -1,26 +1,43 @@
 const params = new URLSearchParams(window.location.search);
-console.log("params:", params);
 const id = params.get("id");
-// console.log("id:", id);
-const productContainer = document.querySelector("#productContainer");
-// console.log("productContainer:", productContainer);
 
 fetch(`https://kea-alt-del.dk/t7/api/products/${id}`)
   .then((response) => response.json())
-  .then((data) => {
-    productContainer.innerHTML = `
-      <figure>
-              <img src="https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp" alt="Produktbillede" class="productImage" />
-              <span class="saleLabel">Udsalg!</span>
-       </figure>
-            <section class="productDetails">
-              <h2 class="productName">${data.variantname}</h2>
-              <div>
-                <p class="articleType"><span class="bold">Type:</span> ${data.articletype}</p>
-                <p class="productCategory"><span class="bold">Kategori:</span> ${data.category}</p>
-                <p class="productPrice"><span class="bold">Pris:</span> ${data.price},-</p>
-              </div>
-              <button class="buyButton">Køb nu</button>
-            </section>
-            `;
+  .then((product) => {
+    showProduct(product);
   });
+
+function showProduct(product) {
+  const productContainer = document.querySelector(".productContainer");
+
+  productContainer.innerHTML = `
+    <article class="product_image">
+      <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}" />
+    </article>
+    <article class="productinformation">
+      <h2>Product information</h2>
+      <h3>Model name:</h3>
+      <p>${product.productdisplayname}</p>
+      <h3>Product type:</h3>
+      <p>${product.articletype}</p>
+      <h3>Brand:</h3>
+      <h1>${product.brandname}</h1>
+    </article>
+    <article class="size_and_purchase">
+      <h2>${product.productdisplayname}</h2>
+      <p>${product.brandname} / ${product.articletype}</p>
+      <p>${product.price},- kr</p>
+      ${product.discount ? `<p>Now DKK ${Math.round(product.price * (1 - product.discount / 100))} ,-</p>` : ""}
+      ${product.soldout ? "<p>SOLD OUT</p>" : ""}
+      <div class="size-row">
+        <label for="size">Choose a size</label>
+        <select id="size">
+          <option>S</option>
+          <option>M</option>
+          <option>L</option>
+          <option>XL</option>
+        </select>
+      </div>
+      <button class="basket-btn">Add to basket</button>
+    </article>`;
+}
